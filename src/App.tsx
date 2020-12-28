24e40f0e2c977c2e8148b854a0756a0178ea5a54
Nicolas Lagier <nicolas.lagier@healint.com>
2020-12-28 20:50:12 +0800

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { Line } from "react-chartjs-2";
+import "./App.css";
+import useAxios from "axios-hooks";
+
+const baseDataset = {
+  label: "My First dataset",
+  backgroundColor: "rgba(255,99,132,0.2)",
+  borderColor: "rgba(255,99,132,1)",
+  borderWidth: 1,
+  hoverBackgroundColor: "rgba(255,99,132,0.4)",
+  hoverBorderColor: "rgba(255,99,132,1)",
+};
 
 function App() {
+  //fetch data
+  let [{ data, loading, error }] = useAxios("edge_api_result_FL_p.json");
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+
+  const graph = {
+    labels: data.data.items.map((item) => item.localtime),
+    datasets: [
+      {
+        ...baseDataset,
+        data: data.data.items.map((item) => item.FL_p),
+      },
+    ],
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Line
+        className="bar"
+        data={graph}
+        width={100}
+        height={500}
+        options={{
+          maintainAspectRatio: false,
+        }}
+      />
     </div>
   );
 }
